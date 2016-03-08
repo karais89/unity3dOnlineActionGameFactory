98b3e1cc33bb3677982abbca1f3f96cf9ea6e5f8
heojihun <karais89@gmail.com>
2016-03-08 22:51:44 +0900

    - 3D 모델 추가

---
+++ Assets/Scripts/RollObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollObject : MonoBehaviour {
+
+	float rollZ;
+
+	// Use this for initialization
+	void Start () {
+		rollZ = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rollZ += Time.deltaTime * 360.0f;
+		transform.rotation = Quaternion.Euler(0, 0, rollZ);
+	}
+}
