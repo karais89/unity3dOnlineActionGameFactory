1f18c18311765e1cf45f8f6bc86ad5abb4556cc6
heojihun <karais89@gmail.com>
2016-03-20 22:00:43 +0900

    - 14 chapter Title Scene Create

--- Assets/Scripts/GameRuleCtrl.cs
+++ Assets/Scripts/GameRuleCtrl.cs
@@ -7,11 +7,26 @@ public class GameRuleCtrl : MonoBehaviour
     public bool gameOver = false;
     public bool gameClear = false;
     
+    // 씬 이행 시간.
+    public float sceneChangeTime = 3.0f;
+    
 	// Update is called once per frame
 	void Update () 
     {
-        timeRemaining -= Time.deltaTime;
         
+        // 게임 종료 조건 성립 후 씬 전환.
+        if(gameOver || gameClear)
+        {
+            sceneChangeTime -= Time.deltaTime;
+            if(sceneChangeTime <= 0.0f)
+            {
+                Application.LoadLevel("titleScene");
+            }
+            return;
+        }
+        
+        timeRemaining -= Time.deltaTime;
+        // 남는 시간이 없어지면 게임 오버.
         if(timeRemaining <= 0.0f)
         {
             GameOver();
---
+++ Assets/Scripts/TitleSceneCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSceneCtrl : MonoBehaviour 
+{
+    // ㅏ타이틀 화면 텍스쳐.
+    public Texture2D bgTexture;
+    
+    void OnGUI()
+    {
+        // 스타일 준비.
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        
+        // 해상도 대응.
+        GUI.matrix = Matrix4x4.TRS(
+          Vector3.zero,
+          Quaternion.identity,
+          new Vector3(Screen.width / 854.0f, Screen.height /480.0f, 1.0f)  
+        );
+        
+        // 타이틀 화면 텍스쳐 표시.
+        GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), bgTexture);
+        
+        // 시작 버튼을 만든다.
+        if(GUI.Button(new Rect(327, 290, 200, 54), "Start", buttonStyle))
+        {
+            // 씬 전환은 Application.LoadLevel을 이용한다.
+            Application.LoadLevel("GameScene");
+        }
+    }
+}
